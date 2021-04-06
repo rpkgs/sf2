3f7eaf7f35f306efe06b6e0eb0de8eeca654e854
Dongdong Kong <kongdd@users.noreply.github.com>
2021-04-06 22:54:31 +0800

    add st_coord

--- R/sf.R
+++ R/sf.R
@@ -4,3 +4,15 @@ st_dissolve <- function(x, by = NULL, ...) {
         dplyr::group_by_at(by) %>%
         dplyr::summarise(...)
 }
+
+#' @importFrom sf st_drop_geometry st_coordinates
+#' @export 
+as.data.table.sf <- function(x) {
+    loc = st_coord(x)
+    shp %>% st_drop_geometry() %>% cbind(loc, .)
+}
+
+#' @export 
+st_coord <- function(x) {
+    st_coordinates(shp) %>% set_colnames(c("lon", "lat"))
+}
