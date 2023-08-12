d9037b5ff7c1d3a9dd0779fc7d8c963f720606e0
Dongdong Kong <kongdd@users.noreply.github.com>
2023-08-12 20:09:59 +0800

    rm rgdal

--- R/read_shp.R
+++ R/read_shp.R
@@ -1,21 +1,10 @@
-#' read shapefile
-#' @param file The path of ESRI shapefile
-#' @param ... other parameters to [rgdal::readOGR()]
-#'
-#' @export
-#'
-#' @seealso [rgdal::readOGR()]
-read_shp <- function(file, ...) {
-    readOGR(file, verbose = FALSE, ...)
-}
-
 #' @importFrom sf as_Spatial st_crs
 #' @export
 read_polyline <- function(file, lwd = 0.5, ...) {
-    if (file.exists(file)) {
-        shp <- read_sf(file) %>% as_Spatial()
-        st_crs(shp) = 4326
-        # suppressWarnings(shp <- readShapeLines(file))
-        list("sp.lines", shp, first = FALSE, lwd = lwd, ...) # china poly
-    }
+  if (file.exists(file)) {
+    shp <- read_sf(file) %>% as_Spatial()
+    st_crs(shp) <- 4326
+    # suppressWarnings(shp <- readShapeLines(file))
+    list("sp.lines", shp, first = FALSE, lwd = lwd, ...) # china poly
+  }
 }
--- R/write_shp.R
+++ R/write_shp.R
@@ -2,18 +2,17 @@
 #'
 #' @param x Spatial* Object from sp package
 #' @param file The path of ESRI shapefile
-#' @param ... other parameters to [rgdal::writeOGR()]
+#' @param ... other parameters to `rgdal::writeOGR()`
 #' 
 #' @export
 write_shp <- function(x, file, ...) {
-    write_sf(sf::st_as_sf(x), file, layer_options = "ENCODING=UTF-8", ...)
+  write_sf(sf::st_as_sf(x), file, layer_options = "ENCODING=UTF-8", ...)
 }
 
-#' @importFrom rgdal writeOGR readOGR
 #' @rdname write_shp
 #' @export
 write_shp_sp <- function(x, file, ...) {
-    writeOGR(x, file, "shape", driver = "ESRI Shapefile", layer_options="ENCODING=UTF-8", ...)
+  rgdal::writeOGR(x, file, "shape", driver = "ESRI Shapefile", layer_options = "ENCODING=UTF-8", ...)
 }
 
 #' gridSaveToPoly
@@ -23,11 +22,9 @@ write_shp_sp <- function(x, file, ...) {
 #'
 #' @param grid The spatialPixelDataframe or spatialGridDataframe object.
 #' @param file The string of output shapefile name including path.
-#' @importFrom rgdal writeOGR
 #' @export
 gridSaveToPoly <- function(grid, file) {
-    poly <- as(grid, "SpatialPolygonsDataFrame")
-    # writePolyShape(poly, file) #, can't write prj info
-    writeOGR(poly, dsn = file, layer = basename(file), driver = "ESRI Shapefile")
+  poly <- as(grid, "SpatialPolygonsDataFrame")
+  # writePolyShape(poly, file) #, can't write prj info
+  rgdal::writeOGR(poly, dsn = file, layer = basename(file), driver = "ESRI Shapefile")
 }
-
