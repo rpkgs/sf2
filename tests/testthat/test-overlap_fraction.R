2088240ee34486b52ad8fd5506a525b26e76a16a
Dongdong Kong <kongdd@users.noreply.github.com>
2025-09-07 19:36:15 +0800

    fix ci

--- tests/testthat/test-overlap_fraction.R
+++ tests/testthat/test-overlap_fraction.R
@@ -1,4 +1,5 @@
 test_that("overlap_fraction works", {
+  load_china()
   d = overlap_fraction(bou1_4p, 1)
   expect_true(abs(sum(d$area2) / 1e6 -  9.490825) <= 1e-5)
   expect_equal(nrow(d), 1140L) 
